81e4ca4a16750dc6899434f23a6a9fb5ca06a55a
Steven Searcy <stevenaaronsearcy@gmail.com>
2021-07-23 16:00:34 -0500

    POST to DB Success

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 
 var indexRouter = require("./routes/index");
 var customerRouter = require("./routes/customer");
 
 var app = express();
 
-var connectionString = "mongodb://127.0.0.1/form_submission_one";
+var connectionString =
+  "mongodb+srv://steven:Password1!@formsubmit.3irda.mongodb.net/Customers?retryWrites=true&w=majority";
 mongoose.connect(
   connectionString,
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -23,6 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 app.use("/api/customer", customerRouter);
--- routes/customer.js
+++ routes/customer.js
@@ -3,21 +3,11 @@ var router = express.Router();
 var customer = require('../models/customer');
 
 router.post('/ordersubmit', async (req, res, next) => {
-  try {
-    console.log(req.body);
-    let newCustomer = new Customer({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      email: req.body.email,
-      phoneNumber: req.body.phoneNumber,
-      address: req.body.address,
-      orderSummary: req.body.orderSummary
-    });
-    console.log(newUser)
-  }
-  catch(err) {
-
-  }
-})
-
+  var myData = new customer(req.body);
+  myData.save().then(item=> {
+    res.send("item saved to db");
+  }).catch(err => {
+    res.status(400).send('unable')
+  });
+});
 module.exports = router;
