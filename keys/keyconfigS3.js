9bc695cba86b06bdef067446d7507dd8afbcc5c2
Steven Searcy <73917010+stevenaaronsearcy@users.noreply.github.com>
2021-08-10 16:25:35 -0500

    Delete keys directory

--- keys/keyconfigS3.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  bucketName: "thenoblecookies",
-  dirName: "CookiePics",
-  region: "us-east-1",
-  accessKeyId: "AKIARRDBSFQ5NOXWEKMT",
-  secretAccessKey: "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",
-};
-
-export default config;
